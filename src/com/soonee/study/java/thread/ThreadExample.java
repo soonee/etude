5e8cb8cc0957a2e1bd61810f13ae52b8f23f5715
soonee <mysiri29@gmail.com>
2022-03-02 00:00:52 +0900

    자바 스레드 테스트 코드 반영 (15장)

---
+++ src/com/soonee/study/java/thread/ThreadExample.java
@@ -0,0 +1,32 @@
+package com.soonee.study.java.thread;
+
+public class ThreadExample extends Thread {
+	int count = 0;
+
+	@Override
+	public void run() {
+		System.out.println("Thread starting.");
+		try {
+			while(count < 5) {
+				Thread.sleep(500);
+				count++;
+				System.out.println("In Thread, count is " + count);
+			}
+		} catch (InterruptedException e) {
+			System.out.println("Thread interrupted.");
+		}
+		System.out.println("Thread terminating.");
+	}
+
+	public static void main(String[] args) {
+		ThreadExample instance = new ThreadExample();
+		instance.start();
+		while(instance.count != 5) {
+			try {
+				Thread.sleep(250);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
