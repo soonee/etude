6839caad06bc441805a0c3f060890269307ffaf4
soonee <mysiri29@gmail.com>
2022-09-23 09:57:04 +0900

    테스트 코드 추가

--- src/com/soonee/test/programmers/level1/DivisorNumbersSum.java
+++ src/com/soonee/test/programmers/level1/DivisorNumbersSum.java
@@ -1,5 +1,9 @@
 package com.soonee.test.programmers.level1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 약수의 합
  */
@@ -20,7 +24,36 @@ public class DivisorNumbersSum {
         return answer;
     }
 
+    public static int test(int n) {
+        double sqrt = Math.sqrt(n);
+        int answer = 0;
+
+        List<Integer> sequences = new ArrayList<>();
+
+        for(int i = 1; i <= sqrt; i++) {
+            if(i * i == n) {
+                answer += i;
+                sequences.add(i);
+                continue;
+            }
+            if(n % i == 0) {
+                answer += i;
+                answer += n / i;
+                sequences.add(i);
+                sequences.add(n / i);
+            }
+        }
+        System.out.printf("given number : %d, square root : %f, number : %s, sum : %d, sequences : %s\n",
+            n, sqrt, sqrt % 1 == 0 ? "정수" : "실수", answer, Arrays.toString(sequences.toArray()));
+        return answer;
+    }
+
     public static void main(String[] args) {
+        test(12);
+        test(5);
+        test(25);
+        test(55);
+        System.out.println("====================");
         System.out.println(solution(12));
         System.out.println(solution(55));
         System.out.println(solution(25));
