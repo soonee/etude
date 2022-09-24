90f85fdc6fcf278252a24a6bfc6d6b9ae4318fe5
soonee <mysiri29@gmail.com>
2022-09-24 23:07:58 +0900

    정수 제곱근 판별 boilerplate code

---
+++ src/com/soonee/test/programmers/level1/IntegerSquareRootDistinguish.java
@@ -0,0 +1,11 @@
+package com.soonee.test.programmers.level1;
+
+public class IntegerSquareRootDistinguish {
+    public static long solution(long n) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        solution(0);
+    }
+}
