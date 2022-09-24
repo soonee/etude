2c3b3a6c2566fc68db9a6524920eb01f5ac5b325
soonee <mysiri29@gmail.com>
2022-09-24 22:58:50 +0900

    프로그래머스 레벨1, 4번째 문제 풀이

--- src/com/soonee/test/programmers/level1/NumberOfDigitAddition.java
+++ src/com/soonee/test/programmers/level1/NumberOfDigitAddition.java
@@ -1,29 +1,30 @@
 package com.soonee.test.programmers.level1;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * 자릿수 더하기
  */
 public class NumberOfDigitAddition {
     public static int solution(int n) {
-        Integer.toString(n);
         char[] digits = Integer.toString(n).toCharArray();
-//        Arrays.asList(digits).stream().mapToInt().sum();
-        int number = 5;
-        String numberOriginString = Integer.toString(number);
-        number = 10;
-        String numberModifyString = Integer.toString(number);
-        if(numberOriginString == numberModifyString) {
-            System.out.println("메모리 할당이 같음");
-        } else {
-            System.out.println("메모리 할당이 다름");
-        }
-        System.out.printf("digits : %s\n", Arrays.toString(digits));
-        return 0;
+        Stream<Character> digitStream = IntStream.range(0, digits.length).mapToObj(index -> digits[index]);
+        return digitStream.map(d -> d - '0').mapToInt(Integer::intValue).sum();
+    }
+    public static int print(int n) {
+        char[] digits = Integer.toString(n).toCharArray();
+        Stream<Character> digitStream = IntStream.range(0, digits.length).mapToObj(index -> digits[index]);
+        int sum = digitStream.map(d -> d - '0').mapToInt(Integer::intValue).sum();
+        System.out.printf("given number : %d, digits : %s, sum : %d\n", n, Arrays.toString(digits), sum);
+        return sum;
     }
 
     public static void main(String[] args) {
-        solution(987);
+        solution(765);
+        solution(5);
+        print(765);
+        print(5);
     }
 }
