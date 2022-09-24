4c32ca1cd1b66474e8a24830e1970a0c47de95bd
soonee <mysiri29@gmail.com>
2022-09-24 23:11:52 +0900

    상용구 코드 수정

--- src/com/soonee/test/programmers/level1/IntegerSquareRootDistinguish.java
+++ src/com/soonee/test/programmers/level1/IntegerSquareRootDistinguish.java
@@ -1,8 +1,14 @@
 package com.soonee.test.programmers.level1;
 
+/**
+ * 정수 제곱근 판별
+ */
 public class IntegerSquareRootDistinguish {
     public static long solution(long n) {
         return 0;
+    }
+    public static void print(long n) {
+        System.out.printf("given number : %d\n", n);
     }
 
     public static void main(String[] args) {
